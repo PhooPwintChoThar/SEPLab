c7661e39a91df534498be076b0aed3b560aa337f
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2025-12-16 17:31:52 +0700

    Update 4.2.py

--- Lab4/4.2.py
+++ Lab4/4.2.py
@@ -4,18 +4,23 @@ from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtMultimedia import QSoundEffect
 
+
 class Animation_area(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
+
         self.frame_no = 0
         self.images = []
+
         for i in range(1, 21):
             pixmap = QPixmap("images/frame_" + str(i + 1) + ".png")
             self.images.append(pixmap)
 
-        timer = QTimer(self)
-        timer.timeout.connect(self.update_value)
-        timer.start(75)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_value)
+        self.timer.start(75)
+
+        self.running = True
 
         self.QSE = QSoundEffect()
         self.QSE.setSource(QUrl.fromLocalFile("sounds/rabbit_jump.wav"))
@@ -33,16 +38,36 @@ class Animation_area(QWidget):
             self.QSE.play()
         self.update()
 
+    def toggle_animation(self):
+        if self.running:
+            self.timer.stop()
+        else:
+            self.timer.start(75)
+        self.running = not self.running
+
 
 class Simple_animation_window(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
+
         self.anim_area = Animation_area()
+
+        self.btn = QPushButton("Pause")
+        self.btn.clicked.connect(self.toggle_play_pause)
+
         layout = QVBoxLayout()
         layout.addWidget(self.anim_area)
-        layout.addWidget(QPushButton("Pause"))
+        layout.addWidget(self.btn)
         self.setLayout(layout)
+
         self.setMinimumSize(330, 400)
+
+    def toggle_play_pause(self):
+        self.anim_area.toggle_animation()
+        if self.anim_area.running:
+            self.btn.setText("Pause")
+        else:
+            self.btn.setText("Play")
 
 
 def main():
